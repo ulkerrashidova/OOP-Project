e980b23f4fe55976d87166bd01e2d08d98a37625
ulkerrashidova <150679004+ulkerrashidova@users.noreply.github.com>
2025-06-26 18:33:41 +0200

    ServicesWindow.xaml.cs

---
+++ ServicesWindow.xaml.cs
@@ -0,0 +1,17 @@
+public partial class ServicesWindow : Window
+    {
+        public ServicesWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
